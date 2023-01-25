99f483770e143e85441ffe2062a47436b023124c
Andrei Nesterov <and.yu.nesterov@gmail.com>
2023-01-25 16:30:02 +0100

    updates

--- LODlitParser/wd.py
+++ LODlitParser/wd.py
@@ -6,36 +6,36 @@ import time
 import re
 
 def get_search_hits(search_term:str,lang:str,user_agent:str,filter_by_keywords='',filter_by_statements='',stemming=False) -> dict:
-	"""
-	Getting search hits of a term in Wikidata
-	search_term: str a term or a phrase to search in Wikidata
-	lang: str with language code; for example, 'en' or 'nl';
+    """
+    Getting search hits of a term in Wikidata
+    search_term: str a term or a phrase to search in Wikidata
+    lang: str with language code; for example, 'en' or 'nl';
     # (see language codes in Wikidata: https://www.wikidata.org/w/api.php?action=help&modules=wbgetentities)
-	user_agent: str with user-agent's info; required by Wikidata (see: https://meta.wikimedia.org/wiki/User-Agent_policy)
-	filter_by_keywords: str optional, set the keywords to exclude from the search results with dashes like "-scientific -article"; default is ''
-	filter_by_statements: str optional; set the statements to exclude from the search results, specifying property and its value like "P31=Q5"
-	# (meaning that entities with the property P31 ("instance of") with the value "Q5" ("human") will be excluded);
-	# multiple statements are possible, separate them with a whitespace; default is ''
-	stemming: bool is to perform stemming of the search term (works only for English); default is False
-	"""
+    user_agent: str with user-agent's info; required by Wikidata (see: https://meta.wikimedia.org/wiki/User-Agent_policy)
+    filter_by_keywords: str optional, set the keywords to exclude from the search results with dashes like "-scientific -article"; default is ''
+    filter_by_statements: str optional; set the statements to exclude from the search results, specifying property and its value like "P31=Q5"
+    # (meaning that entities with the property P31 ("instance of") with the value "Q5" ("human") will be excluded);
+    # multiple statements are possible, separate them with a whitespace; default is ''
+    stemming: bool is to perform stemming of the search term (works only for English); default is False
+    """
 
-	search_hits = {}
+    search_hits = {}
 
-	# 'query' with 'search' generator: constant params
-	url = "https://www.wikidata.org/w/api.php"
-	params = {"action":"query",
-	          "prop":"entityterms",
-	          "wbetlanguage":lang,
-	          "generator":"search",
-	          "gsrsearch":f'"{search_term}"', # term goes here (quotes for stemming off)
-	          "gsrlimit":"1", # 1 result is enough to get the hits info
-	          "gsrinfo":"totalhits",
-	          "format":"json"}
-	headers = {"user-agent":user_agent}
+    # 'query' with 'search' generator: constant params
+    url = "https://www.wikidata.org/w/api.php"
+    params = {"action":"query",
+              "prop":"entityterms",
+              "wbetlanguage":lang,
+              "generator":"search",
+              "gsrsearch":f'"{search_term}"', # term goes here (quotes for stemming off)
+              "gsrlimit":"1", # 1 result is enough to get the hits info
+              "gsrinfo":"totalhits",
+              "format":"json"}
+    headers = {"user-agent":user_agent}
 
-	# stemming
-	if stemming == True:
-		params["gsrsearch"] = search_term
+    # stemming
+    if stemming == True:
+        params["gsrsearch"] = search_term
 
     r = requests.get(url,params=params,headers=headers)
     hits = r.json()['query']['searchinfo']['totalhits']
